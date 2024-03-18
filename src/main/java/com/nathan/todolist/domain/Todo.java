c376ea693137305006029c99791357634d66221f
Nathan Paiva <nathanpaiva00@gmail.com>
2024-03-18 08:25:13 -0300

    Project Created

---
+++ src/main/java/com/nathan/todolist/domain/Todo.java
@@ -0,0 +1,78 @@
+package com.nathan.todolist.domain;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Todo {
+	private Integer id;
+	private String titulo;
+	private String descricao;
+	private LocalDateTime dataParaFinalizar;
+	private boolean finalizado = false;
+	
+	public Todo() {
+		super();
+	}
+	
+	public Todo(Integer id, String titulo, String descricao, LocalDateTime dataParaFinalizar, boolean finalizado) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.descricao = descricao;
+		this.dataParaFinalizar = dataParaFinalizar;
+		this.finalizado = finalizado;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public LocalDateTime getDataParaFinalizar() {
+		return dataParaFinalizar;
+	}
+	public void setDataParaFinalizar(LocalDateTime dataParaFinalizar) {
+		this.dataParaFinalizar = dataParaFinalizar;
+	}
+	
+
+	public boolean isFinalizado() {
+		return finalizado;
+	}
+	public void setFinalizado(boolean finalizado) {
+		this.finalizado = finalizado;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Todo other = (Todo) obj;
+		return Objects.equals(id, other.id);
+	}
+}

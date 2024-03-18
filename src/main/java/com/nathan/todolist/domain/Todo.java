d25013ed8155b01279fe586e72d37fff48ff501a
Nathan Paiva <nathanpaiva00@gmail.com>
2024-03-18 09:17:11 -0300

    Domain class finalization - 2

--- src/main/java/com/nathan/todolist/domain/Todo.java
+++ src/main/java/com/nathan/todolist/domain/Todo.java
@@ -1,11 +1,22 @@
 package com.nathan.todolist.domain;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-public class Todo {
+@Entity
+public class Todo implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
 	private String titulo;
 	private String descricao;
 	private LocalDateTime dataParaFinalizar;
